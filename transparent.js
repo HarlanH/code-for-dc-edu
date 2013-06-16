366b5da212c4eeed9cef3a93e0b462412643e5c3
Harlan Harris <harlan@harris.name>
2013-06-15 23:10:56 -0400

    schools map display reflects counts; resembles neighborhood
    Should probably consider drawing lines, adding mouseover like Neighborhood.
    Rearranged JS a bit.

--- transparent.js
+++ transparent.js
@@ -61,63 +61,6 @@ function getSchools(clusterId) {
   return schools;
 }
 
-var schoolmarker = new Array(); 
-var geojson;
-function schoolListSelected() {
-  var myselect = document.getElementById("schoolsList");
-  var schoolID = myselect.options[myselect.selectedIndex].id;
-  var clusters = getClusters(schoolID);
-  // clusters[1].lat and .lon should work
-  //console.log(clusters);
-  var first_cluster = clusters[Object.keys(clusters)[0]];
-
-  if(schoolmarker.length > 0){
-      map.removeLayer(geojson);
-  }
-  for(i=0;i<schoolmarker.length;i++) {
-      map.removeLayer(schoolmarker[i]);
-      schoolmarker.splice(i,1);
-  }  
-   displayClusters(schoolID);
-   //console.log(first_cluster);
-   var TempMarker = new L.marker([first_cluster.lat, first_cluster.lon]);
-   schoolmarker.push(TempMarker);
-   //schoolmarker[0].bindPopup(schoolID);
-   map.addLayer(schoolmarker[0]);
-}
-function displayClusters(schoolId)
-{
-    $.getJSON('clusters.geojson', function(data){
-    geojson = L.geoJson(data, {
-            style: function (feature) {
-                var clusters = [];
-
-                clusters = getClusters(schoolId);
-                var id = parseInt(feature.properties.GIS_ID.substring(8));
-
-                if(id in clusters  )
-                {
-                    if(clusters[id].count >0)
-                    {
-                        return {color: 'blue'};
-                    }
-                    else
-                    {
-                        return {color:'red'}
-                    }
-
-                }
-                else
-                {
-                    return {color:'red'}
-                }
-            }      
-        }).addTo(map);
-
-        console.log(geojson);
-
-    });
-}
 
 var nc_centers;
 $.getJSON('data/nc_names_centers.json', function(data) {
