6285a059cdbfd8a6607a4fb4a3cae2446ff676b6
Sam Leitner <sam.leitner@gmail.com>
2013-06-19 16:24:07 -0400

    added a legend and coloring for the neighborhoods map, reset of the line colors is broken.

--- neighborhood-postload.js
+++ neighborhood-postload.js
@@ -71,13 +71,44 @@ function resetLine(e) {
         color: 'blue'
     });
 }
+var grades = [0, 10, 20, 50, 80, 120, 150];
+function getColor(d) {
+    return d > grades[6] ? '#800026':
+           d > grades[5] ? '#BD0026':
+           d > grades[4] ? '#E31A1C':
+           d > grades[3] ? '#FC4E2A':
+           d > grades[2] ? '#FD8D3C':
+           d > grades[1] ? '#FEB24C':
+                           '#FFEDA0';
+}
+//           d > grades[1] ? '#FED976':
+function getColorR(d) {
+    return d > grades[6] ? '#FFEDA0':
+           d > grades[5] ? '#FEB24C':
+           d > grades[4] ? '#FD8D3C':
+           d > grades[3] ? '#FC4E2A':
+           d > grades[2] ? '#E31A1C':
+           d > grades[1] ? '#BD0026':
+                           '#800026';
+//           d > grades[6] ? '#FED976':
+}
+function line_opacity(d){
+    return d<2   ? 0.3 : 
+        d<100 ? 0.3 + (d/100.0)*0.4 : 
+         0.7 ;
+}
+
 var school_lines = new Array(); 
 function displaySchools(e) {
 	var layer = e.target;
 
+	if (school_lines.length > 0) {
+            legend.removeFrom(neighmap);
+        }
+        legend.addTo(neighmap);
 	// wipe any old school lines
 	while (school_lines.length > 0) {
-		neighmap.removeLayer(school_lines.pop())
+            neighmap.removeLayer(school_lines.pop());
 	}
 	// get the schools for this cluster
 	var cluster_id = parseInt(layer.feature.properties.GIS_ID.substring(8));
@@ -91,8 +122,9 @@ function displaySchools(e) {
 			//marker.addTo(neighmap);
 			var lineseg = L.polyline([[schools[i].lat, schools[i].lon], 
 				[nc_centers.lat_ctr[cluster_id-1], nc_centers.lon_ctr[cluster_id-1]]],
-				{ weight: 1+((schools[i].count<10)?0:Math.sqrt(schools[i].count)),
-				  opacity: .5, color: 'blue',
+				{ weight: 3+((schools[i].count<10)?0:Math.sqrt(schools[i].count/4.0)),
+				  opacity: line_opacity(schools[i].count), 
+                                  color: getColor(schools[i].count),
 				  txt: schools[i].school_name + ": " + ((schools[i].count<10)?"few":schools[i].count) + " students" });
 			lineseg.addTo(neighmap);
 			lineseg.on({mouseover: highlightLine, mouseout: resetLine});
@@ -112,4 +144,21 @@ var geojson;
 $.getJSON('clusters.geojson', function(data){
     geojson = L.geoJson(data, {style: style, onEachFeature: onEachFeature}).addTo(neighmap);
 });
+
+
+// http://leafletjs.com/examples/choropleth.html
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (neighmap) {
+    var div = L.DomUtil.create('div', 'info legend'),
+    labels = [];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+
+
 
